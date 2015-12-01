b82b876761a878b3e84766bf89fc2a557d331643
André Padez <andre.padez@gmail.com>
2015-12-01 01:05:28 +0100

    app ready

--- json/index.js
+++ json/index.js
@@ -1,31 +1,60 @@
 var $ = require('jquery')
+require('./style.less')
 
 var dataURL = 'http://espn.go.com/users/recruiting/fantasy-sample-data.json'
 $.getJSON(dataURL, function(json){
 	
 	var players = json.players
-	console.log(players)
-	for (var i = 0; i <= players.length; i++) {
+	console.log(json)
+	
+	for (var i = 0; i < players.length; i++) {
 		var player = players[i].player
-		console.log('player', i, player)
-
-		var playerElement = document.createElement('div')
-		var h3 = document.createElement('h3')
-		h3.innerText = player['player-name']
-		playerElement.appendChild(h3)
-
-		var stats = player['player-stats']
-		var statsKeys = Object.keys(stats)
-
-		for(var j = 0; j < statsKeys.length; j++){
-			var key = statsKeys[j]
-			playerElement.innerHTML += key + ': ' + stats[key] + '<br>'
-		}
-
-		console.log(statsKeys)
-
-
-
-		document.body.appendChild(playerElement)
+		player['player-stats'].score = calculateScores(player)
+		players[i] = player
 	}
+	players = orderPlayers(players)
+	renderPlayers(players)
 })
+
+function orderPlayers(players){
+	return players.sort(function(a, b){
+		var aScore = a['player-stats'].score
+		var bScore = b['player-stats'].score
+		return aScore < bScore ? 1 : -1
+	})
+}
+
+function calculateScores(player){
+	var stats = player['player-stats']
+	var score = 0
+	score += stats['passing-touchdowns'] * 4
+	score += Math.floor(stats['passing-yards'] / 25)
+	score += stats['rushing-touchdowns'] * 6
+	score += Math.floor(stats['rushing-yards'] / 10)
+	score += stats['receiving-touchdowns'] * 6
+	score += Math.floor(stats['receiving-yards'] / 10)
+	return score
+}
+
+function renderPlayers(players){
+	for (var i = 0; i < players.length; i++) {
+		renderPlayer(players[i])
+	}
+}
+
+function renderPlayer(player){
+	var playerElement = document.createElement('div')
+	var h3 = document.createElement('h3')
+	h3.innerText = player['player-name']
+	playerElement.appendChild(h3)
+
+	var stats = player['player-stats']
+	var statsKeys = Object.keys(stats)
+
+	for(var j = 0; j < statsKeys.length; j++){
+		var key = statsKeys[j]
+		playerElement.innerHTML += key + ': ' + stats[key] + '<br>'
+	}
+
+	document.body.appendChild(playerElement)
+}
